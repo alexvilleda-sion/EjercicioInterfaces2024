d7069e3c03cad594160b771f650ea614804bf163
Alex-Villeda <152237578+Alex-Villeda@users.noreply.github.com>
2024-10-29 20:47:53 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioInterfaces2024/Operaciones.cs
@@ -0,0 +1,22 @@
+﻿public class Operaciones : IOperaciones
+{
+    public double CalcularAreaCirculo(double radio) => Math.PI * Math.Pow(radio, 2);
+
+    public double CalcularAreaRectangulo(double largo, double ancho) => largo * ancho;
+
+    public double CalcularVolumenCubo(double lado) => Math.Pow(lado, 3);
+
+    public double CalcularPerimetroCuadrado(double lado) => 4 * lado;
+
+    public double CalcularVolumenEsfera(double radio) => (4 / 3) * Math.PI * Math.Pow(radio, 3);
+
+    public void Imprimir()
+    {
+        Console.WriteLine("Resultados de las operaciones:");
+        Console.WriteLine($"Área del círculo: {CalcularAreaCirculo(5)}");
+        Console.WriteLine($"Área del rectángulo: {CalcularAreaRectangulo(4, 6)}");
+        Console.WriteLine($"Volumen del cubo: {CalcularVolumenCubo(3)}");
+        Console.WriteLine($"Perímetro del cuadrado: {CalcularPerimetroCuadrado(7)}");
+        Console.WriteLine($"Volumen de la esfera: {CalcularVolumenEsfera(4)}");
+    }
+}
