a5427b35f4da75ea9add89fc0a0e3e1a7846dbb5
alexvilleda-sion <100446107+alexvilleda-sion@users.noreply.github.com>
2024-11-10 15:10:15 -0600

    Add files via upload

---
+++ IOperaciones.cs
@@ -0,0 +1,8 @@
+﻿public interface IOperaciones
+{
+    double CalcularAreaCirculo(double radio);
+    double CalcularAreaRectangulo(double largo, double ancho);
+    double CalcularVolumenCubo(double lado);
+    double CalcularPerimetroCuadrado(double lado);
+    double CalcularVolumenEsfera(double radio);
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Operaciones operaciones = new Operaciones();
+
+        Console.WriteLine("Ingrese el radio del círculo:");
+        double radioCirculo = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Área del círculo: {operaciones.CalcularAreaCirculo(radioCirculo)}");
+
+        Console.WriteLine("Ingrese el largo del rectángulo:");
+        double largo = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el ancho del rectángulo:");
+        double ancho = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Área del rectángulo: {operaciones.CalcularAreaRectangulo(largo, ancho)}");
+
+        operaciones.Imprimir();
+    }
+}
